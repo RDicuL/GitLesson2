80dcb41c2513570190f1cb159812062f32093f45
RDicuL <malaho2014@mail.ru>
2021-05-28 15:14:29 +0300

    added function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    public static void branch4(){
+
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
